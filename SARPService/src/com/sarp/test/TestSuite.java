86e378e657965eb7d6e9c9e5c0603c7ec79669d2
Matias Heredia <matisanba@gmail.com>
2016-10-01 03:11:28 -0200

    test unitario abm display

---
+++ SARPService/src/com/sarp/test/TestSuite.java
@@ -0,0 +1,13 @@
+package com.sarp.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+	DisplayTest.class,
+})
+
+public class TestSuite {   
+}  	
