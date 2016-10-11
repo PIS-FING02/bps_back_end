cb8d9bd21716ee3733d3f6c01ef15de7feb09db6
Francisco Cabrera <francisco.cabrera@fing.edu.uy>
2016-10-11 16:33:11 -0300

    Se agrega properties a GAFU y EMFactory

--- SARPDAO/src/com/sarp/dao/factory/EMFactory.java
+++ SARPDAO/src/com/sarp/dao/factory/EMFactory.java
@@ -1,17 +1,45 @@
 package com.sarp.dao.factory;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class EMFactory {
-	
+
 	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("postgresUnit");
-		
-	/* Se trabaja con un único EntityManagerFactory que crea varios EntityManager, uno por transacción */
+	/*
+	 * Se trabaja con un ï¿½nico EntityManagerFactory que crea varios
+	 * EntityManager, uno por transacciï¿½n
+	 */
 	public static EntityManager getEntityManager() {
-	    return emf.createEntityManager();
+		
+		/*String path_os;
+		String[] result;
+		String path;
+		String location = EMFactory.class.getProtectionDomain().getCodeSource().getLocation().getPath().toString();
+		result = location.split("/standalone");
+		path_os = "/modules/conf/config_base.properties";
+		path= result[0] + path_os;
+		Properties prop = new Properties();
+		InputStream input;
+		try {
+			input = new FileInputStream(path);
+			prop.load(input);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		emf = Persistence.createEntityManagerFactory("postgresUnit", prop);*/
+		
+		return emf.createEntityManager();
 	}
-	   	
-	
+
 }
