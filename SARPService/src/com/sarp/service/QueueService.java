b90a520b3d5ca8291c5ffa83d347c34b1d093f3d
marcelo.rydel <marcelo.rydel@fing.edu.uy>
2016-11-05 15:32:35 -0300

    Sigo con logging de errores

--- SARPService/src/com/sarp/service/QueueService.java
+++ SARPService/src/com/sarp/service/QueueService.java
@@ -1,13 +1,7 @@
 package com.sarp.service;
 
-import java.util.List;
-
 import javax.enterprise.context.RequestScoped;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
+
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
